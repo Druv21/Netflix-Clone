b4bb452460954f8edbc46db60940a0b62dba9fb4
Druv21 <dhruvag911@gmail.com>
2022-07-21 19:24:46 +0530

    navbar and featured section completed

---
+++ src/components/featured/Featured.js
@@ -0,0 +1,54 @@
+import { InfoOutlined, PlayArrow } from '@material-ui/icons'
+import React from 'react'
+import './featured.css'
+
+const Featured = ({type}) => {
+  return (
+    <div className="featured">
+        {type && (
+            <div className="category">
+                <span>{type==="movie"?"Movies":"Series"}</span>
+                <select name="genre" id="genre">
+                    <option>Genre</option> 
+                    <option value="adventure">Adventure</option> 
+                    <option value="comedy">Comedy</option> 
+                    <option value="crime">Crime</option> 
+                    <option value="fantasy">Fantasy</option> 
+                    <option value="historical">Historical</option> 
+                    <option value="horror">Horror</option> 
+                    <option value="romance">Romance</option> 
+                    <option value="sci-fi">Sci-fi</option> 
+                    <option value="thriller">Thriller</option> 
+                    <option value="western">Western</option> 
+                    <option value="animation">Animation</option> 
+                    <option value="drama">Drama</option> 
+                    <option value="documentary">Documentary</option>
+                </select>
+            </div>
+        )}
+        <img src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt=""/>
+        <div className="info">
+            <img src="https://occ-0-1361-2567.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABeeMILAyu161f6TSuh-poQWNKfIo_pl9aeifrQ7noJsTYX78AbLCLDsPzCHSp7N0OfCGrU3nGDFtynNnZ3lKQbXuPgpbRObPUCcAPFgwtp2K.png?r=df1" alt=""/>
+            <span className="desc">
+                lorem ispum vyjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+                jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+                jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjcvctctzss
+                sssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+                sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+            </span>
+            <div className="buttons">
+                <button className="play">
+                    <PlayArrow/>
+                    <span>Play</span>
+                </button>
+                <button className="more">
+                    <InfoOutlined/>
+                    <span>Info</span>
+                </button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Featured
---
+++ src/home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./home.css"
+import Navbar from "../components/navbar/Navbar"
+import Featured from '../components/featured/Featured'
+
+const Home = () => {
+  return (
+    <div className="home">
+        <Navbar/>
+        <Featured type="series"/>
+    </div>
+  )
+}
+
+export default Home
