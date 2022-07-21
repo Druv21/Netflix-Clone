d0a647b5b13a107f3bbec0bd02088472667ec4e1
Saumya Chaudhary <chaudharysaumya25@gmail.com>
2022-07-21 22:28:14 +0530

    Added authentication and user routes

---
+++ api/index.js
@@ -0,0 +1,20 @@
+const express=require('express')
+const app=express()
+const mongoose=require('mongoose')
+const dotenv=require('dotenv')
+const authRoute=require('./routes/auth')
+const userRoute=require('./routes/users')
+
+dotenv.config()
+
+
+mongoose.connect(process.env.MONGODB_URL).then(()=>console.log('connected to db'))
+.catch((err)=>console.log(err))
+
+app.use(express.json())
+app.use('/api/auth',authRoute)
+app.use('/api/users',userRoute)
+
+app.listen(8000,()=>{
+    console.log('Server is running!')
+})
