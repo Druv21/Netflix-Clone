90e48e383279174046fd6b9bda2c114cffb632e0
Saumya Chaudhary <chaudharysaumya25@gmail.com>
2022-07-22 15:57:55 +0530

    Completed movies routes

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const mongoose=require('mongoose')
 const dotenv=require('dotenv')
 const authRoute=require('./routes/auth')
 const userRoute=require('./routes/users')
+const movieRoute=require('./routes/movies')
 
 dotenv.config()
 
@@ -14,6 +15,7 @@ mongoose.connect(process.env.MONGODB_URL).then(()=>console.log('connected to db'
 app.use(express.json())
 app.use('/api/auth',authRoute)
 app.use('/api/users',userRoute)
+app.use('/api/movies',movieRoute)
 
 app.listen(8000,()=>{
     console.log('Server is running!')
