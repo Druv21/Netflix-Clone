181c6134b529d173dca07b4bf52e8d6186ab7ee0
Druv21 <dhruvag911@gmail.com>
2022-07-24 14:33:16 +0530

    Completed UI design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Home from './pages/home/Home';
 import Watch from './pages/watch/Watch';
-import Register from './pages/register/Register'
+import Register from './pages/register/Register';
+import Login from './pages/login/Login';
 
 function App() {
   return (
-    <Register/>
+    <Home/>
   );
 }
 
