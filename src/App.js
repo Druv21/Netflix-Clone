8b053f44e8843cf11d9190b6cd18d4e09be223bd
Druv21 <dhruvag911@gmail.com>
2022-07-25 11:21:22 +0530

    Done linking of several pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
-import './App.css';
-import Home from './pages/home/Home';
-import Watch from './pages/watch/Watch';
-import Register from './pages/register/Register';
-import Login from './pages/login/Login';
+import "./App.css";
+import Home from "./pages/home/Home";
+import Watch from "./pages/watch/Watch";
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 function App() {
+  const user = false;
+
   return (
-    <Home/>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {user ? <Home /> : <Redirect to="/register" />}
+        </Route>
+        <Route path="/register">
+          {!user ? <Register /> : <Redirect to="/" />}
+        </Route>
+        <Route path="/login">{!user ? <Login /> : <Redirect to="/" />}</Route>
+        {user ? (
+          <>
+            <Route path="/movies">
+              <Home type="movies" />
+            </Route>
+            <Route path="/series">
+              <Home type="series" />
+            </Route>
+            <Route path="/watch">
+              <Watch />
+            </Route>
+          </>
+        ) : (
+          <>
+            <Redirect to="/login" />
+          </>
+        )}
+      </Switch>
+    </Router>
   );
 }
 
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./home.css"
 import Navbar from "../../components/navbar/Navbar"
 import Featured from '../../components/featured/Featured'
 import List from '../../components/list/List'
 
-const Home = () => {
+const Home = ({type}) => {
+
+  const [lists,setLists]=useState([]);
+
+  
+
   return (
     <div className="home">
         <Navbar/>
-        <Featured />
+        <Featured type={type}/>
         <List/>
         <List/>
         <List/>
